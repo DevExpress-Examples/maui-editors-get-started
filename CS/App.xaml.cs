a3d0026982a2b7124c54491dcfaf4f449708c1c7
nevse <nov.eugen@gmail.com>
2025-01-17 17:18:25 +0400

    net9

--- CS/App.xaml.cs
+++ CS/App.xaml.cs
@@ -10,8 +10,11 @@ namespace EditorsGetStarted
 		public App()
 		{
 			InitializeComponent();
-
-			MainPage = new MainPage();
 		}
-	}
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            return new Window(new MainPage());
+        }
+    }
 }
